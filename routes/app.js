0dcb2d03c50c19ed6603e8c8d4facf68e018b193
imrebartis <imbartis@gmail.com>
2017-07-10 13:32:49 +0300

    using req.params and the post method to update the message variable both in the url and in the html document

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message', function (req, res, next) { //this will render the node view
-    res.render('node', { message: 'Halöchen'});
+router.get('/message/:msg', function (req, res, next) {
+    res.render('node', { message: req.params.msg});
+});
+
+router.post('/message', function (req, res, next) { //outputing the message that is now submittable by user in the node view's form
+    var message = req.body.message;
+    res.redirect('/message/' + message);
 });
 
 module.exports = router;
