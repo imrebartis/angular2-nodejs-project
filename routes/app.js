64c518407484c72cad76985199dcda83febe082b
imrebartis <imbartis@gmail.com>
2017-07-11 09:34:46 +0300

    modified the get route in order to display the email in node.hbs

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,13 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    User.findOne({}, function(err, doc){ //the {} will get the first one in the db
+        if(err) {
+            return res.send('Error');
+        }
+        res.render('node', {email: doc.email});
+    }); 
+ 
 });
 
 router.post('/', function (req, res, next) { //creating the user resource   
