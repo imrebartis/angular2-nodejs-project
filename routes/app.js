21166e2a5954b7398d6d335d93803d46d046fbf9
imrebartis <imbartis@gmail.com>
2017-07-11 14:39:47 +0300

    cleaned up the app.js route

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
-router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){ //the {} will get the first one in the db
-        if(err) {
-            return res.send('Error');
-        }
-        res.render('node', {email: doc.email});
-    }); 
- 
-});
-
-router.post('/', function (req, res, next) { //creating the user resource   
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Jim',
-        lastName: 'B',
-        password: "s",
-        email: email
-    });
-    user.save(); //saving the data into the users collection of our db
-    res.redirect('/');
+router.get('/', function (req, res, next) { 
+        res.render('index');
 });
 
 module.exports = router;
