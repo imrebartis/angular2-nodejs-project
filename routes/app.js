b532343fea8e4998e7e750a17a1b991d0e8a1c30
imrebartis <imbartis@gmail.com>
2017-07-11 09:23:46 +0300

    created a user resource in app.js and saved it to the db with the email submitted in the node.hbs form

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', { message: req.params.msg});
-});
-
-router.post('/message', function (req, res, next) { //outputing the message that is now submittable by user in the node view's form
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+router.post('/', function (req, res, next) { //creating the user resource   
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Jim',
+        lastName: 'B',
+        password: "s",
+        email: email
+    });
+    user.save(); //saving the data into the users collection of our db
+    res.redirect('/');
 });
 
 module.exports = router;
