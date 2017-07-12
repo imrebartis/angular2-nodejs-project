b69515a2782c098f8c2af6d94881d57ae4726ded
imrebartis <imbartis@gmail.com>
2017-07-12 19:52:36 +0300

    created the header component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,14 +5,20 @@ import {FormsModule} from '@angular/forms'; // ngModel in the html needs this
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent} from "./messages/message-input.component";
+import { MessageInputComponent } from "./messages/message-input.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthenticationComponent } from './auth/authentication.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule], // ngModel in the html needs FormsModule
     bootstrap: [AppComponent]
---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component( {
+    selector: 'app-authentication',
+    template: '<h1>auth</h1>'
+})
+
+export class AuthenticationComponent {
+
+}
