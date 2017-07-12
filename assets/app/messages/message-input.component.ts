17b0b6ca506936836a4b559abec3a7f00c5884c7
imrebartis <imbartis@gmail.com>
2017-07-12 15:17:45 +0300

    MessageService added to message-input ts, add message works

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,13 +1,22 @@
 import { Component } from "@angular/core";
+import { MessageService } from './message.service';
 import { Message } from './message.model';
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: 'message-input.component.html'
+    templateUrl: 'message-input.component.html',
+    providers: [
+        MessageService //providers lets us use this service only in this component, see class below
+    ]
 })
 
 export class MessageInputComponent {
+
+    constructor (private messageService: MessageService) {
+
+    }
     onSave(value: string) {
-        console.log(value)
+        const message = new Message(value, 'Bjim');
+        this.messageService.addMessage(message);
     }
 }
