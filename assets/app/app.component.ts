4ceed326352e0b52d8a714e42ad8cd66fe4c57b7
imrebartis <imbartis@gmail.com>
2017-07-12 08:46:33 +0300

    using *ngFor to output a list of messages

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -6,5 +6,8 @@ import { Message } from './messages/message.model';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    message: Message = new Message('hi', 'Bjim'; //assigning content of message thanks to @Input() used in message.component.ts
+    messages: Message[] = [
+        new Message('hi', 'Bjim'; //assigning content of message thanks to @Input() used in message.component.ts
+        new Message('ho', 'Bjam';
+        new Message('hey', 'Bjom';
 }
