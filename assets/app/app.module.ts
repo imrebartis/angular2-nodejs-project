cb9dc9ae3bc5e7f1ab45131080190e653a49dae5
imrebartis <imbartis@gmail.com>
2017-07-13 09:03:29 +0300

    signin & signup htmls and ts created

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -8,6 +8,9 @@ import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from './auth/authentication.component';
+import { LogoutComponent } from './auth/logout.component';
+import { SignupComponent } from './auth/signup.component';
+import { SigninComponent } from './auth/signin.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app.routing';
 
@@ -19,7 +22,10 @@ import { routing } from './app.routing';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SignupComponent,
+        SigninComponent
     ],
     imports: [BrowserModule, FormsModule, routing], // ngModel in the html needs FormsModule
     bootstrap: [AppComponent]
